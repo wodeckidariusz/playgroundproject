4e14723574766901a414ed39396b7fdf0e9e1a17
wodeckidariusz <dariuszwodecki82@gmail.com>
2021-11-17 20:59:24 +0100

    Initial project creation

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PlaygroundProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Animal
+    {
+        private string _animalAge;
+        public string AnimalAge
+        {
+            get { return _animalAge; }
+            set { _animalAge = value; }
+        }
+    }
+}
