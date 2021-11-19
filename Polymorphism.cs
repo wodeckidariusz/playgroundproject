b95e6570185b337386dd886c6087879a24d7d265
Dariusz W <dariuszwodecki82@gmail.com>
2021-11-19 11:28:11 +0100

    Polymorphism; Abstractions

---
+++ Polymorphism.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace PlaygroundProject
+{
+    public class Animal
+    {
+        private int _animalAge;
+
+        public Animal(int age)
+        {
+            _animalAge = age;
+        }
+        public int AnimalAge
+        {
+            get { return _animalAge; }
+            set { _animalAge = value; }
+        }
+        public virtual void makeNoise()
+        {
+            Console.WriteLine("Generic animal's noise: sssss");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        private int _animalAge;
+        private string _behavior;
+
+        public Cat(int age, string behavior) : base(age)
+        {
+            _animalAge = age;
+        }
+        public string Behavior
+        {
+            get { return _behavior; }
+            set { Behavior = _behavior; }
+        }
+
+        public override void makeNoise()
+        {
+            Console.WriteLine("Cat's noise: meowwww!");
+        }
+    }
+
+    public class Wolf : Animal
+    {
+        private int _animalAge;
+        private string _behavior;
+
+        public Wolf(int age, string behavior) : base(age)
+        {
+            _animalAge = age;
+        }
+        public string Behavior
+        {
+            get { return _behavior; }
+            set { Behavior = _behavior; }
+        }
+
+        public override void makeNoise()
+        {
+            Console.WriteLine("Wolf's noise: auuuuuu!");
+        }
+    }
+
+    public class Dog : Wolf
+    {
+        public Dog(int age, string behavior) : base(age, behavior)
+        {
+
+        }
+
+        public override void makeNoise()
+        {
+            Console.WriteLine("Dog's noise: wow wow!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,22 +6,38 @@ namespace PlaygroundProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            #region Polymorphism
 
-    public class Animal
-    {
-        private string _animalAge;
+            /*Animal animal = new Animal(10);
+            Animal dog = new Dog(12, "Owner's best friend.");
+            Animal cat = new Cat(5, "Catching mice.");
 
-        public Animal()
-        {
-            AnimalAge = _animalAge;
-        }
-        public string AnimalAge
-        {
-            get { return _animalAge; }
-            set { _animalAge = value; }
+            //Cat cat1 = new Animal(12); -> Wrong while parent (base) class doesn't inherit from child (Cat) class
+
+            animal.makeNoise();
+            dog.makeNoise();
+            cat.makeNoise();
+
+            Dog pureDog = new Dog(12, "Owner's best friend.");
+            Cat pureCat = new Cat(12, "Catching mice.");
+            Wolf pureWolf = new Wolf(4, "Hunter.");
+
+            animal = (Animal)pureDog; //upcasting - not always possible
+            pureWolf = (Wolf)animal; //downcasting - always possible
+
+            Animal animal1 = new Dog(12, "Sth");
+            Wolf wolf1 = (Wolf)animal1;
+            Dog dog1 = (Dog)wolf1;
+            dog1.makeNoise();*/
+
+            #endregion
+
+            #region Abstractions
+
+            Stock stock = new Stock(12, 33);
+            stock.display(); //calling abstract method
+
+            #endregion
         }
     }
 }
